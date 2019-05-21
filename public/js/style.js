8e5cc2459be2e3cf594db4f5d5d105cd793d6044
HermesCat <jppettis@comcast.net>
2019-05-21 18:47:17 -0400

    commiting my front-end stuff

--- public/js/style.js
+++ public/js/style.js
@@ -1,5 +1,29 @@
+
+var signedIn = false;
+
+
 $(document).ready(function () {
     if (!$.browser.webkit) {
         $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
     }
 });
+
+
+function signinCheck(signedIn) {
+    if (signedIn === false) {
+        $("#buttonList").append().html(
+            `<div id="welcome">
+            <button data-toggle="modal" data-target="#loginModal" style="display:show"
+              id="loginButton">
+              Login
+            </button>
+            <a href= "signup.html"><button type="button" class="hot"
+              id="signUpButton">Sign-Up</button></a>
+          </div>`
+        );
+    } else {
+        console.log("cool");
+    }
+}
+
+signinCheck(signedIn);
