25fa8d8b9ced2ec8bd047713c993c83ed8552e5f
SBC-VN <vnestohr@juno.com>
2019-05-21 23:01:56 -0400

    Login button code.

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,19 @@
+var loggedIn = false;
+var screenName = null;
 
+$("#login").on("click",function(event) {
+    var email = $("#emailInputLogin").val();
+    var password = $("#passwordLogin").val();
+
+    $.ajax("/api/login/" + email, {
+        type: "GET",
+        data: { password : password }
+      }).then(
+        function(res) {
+          console.log("login response",res);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    // Call the login api
+    });
