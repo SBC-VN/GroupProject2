2c05e7fd30dd2398e864bdbdd66fbe476db14f37
HermesCat <jppettis@comcast.net>
2019-05-27 19:36:55 -0400

    checking my commit status 5/27

--- models/match.js
+++ models/match.js
@@ -1,35 +1,36 @@
 module.exports = function(sequelize, DataTypes) {
-    var Match = sequelize.define("Match", {
+  var Match = sequelize.define(
+    "Match",
+    {
       // A match is 'owned' by one user, but has an association to another.
-        deltascore:  
-        { type: DataTypes.INTEGER,
-          allowNull: false}
+      deltascore: { type: DataTypes.INTEGER, allowNull: false }
     },
     {
       indexes: [
-          {
-              unique: true,
-              fields: ['user1', 'user2']
-          }
+        {
+          unique: true,
+          fields: ["user1", "user2"]
+        }
       ]
-    });
-  
-    Match.associate = function(models) {
-      Match.belongsTo(models.User, {
-        as: 'userInfo1',
-        foreignKey: {
-          name: 'user1',
-          allowNull: false
-         }
-      });
-      Match.belongsTo(models.User, {
-        as: 'userInfo2',
-        foreignKey: {
-          name: 'user2',
-          allowNull: false
-         }
-      });
     }
+  );
 
-    return Match;
+  Match.associate = function(models) {
+    Match.belongsTo(models.User, {
+      as: "userInfo1",
+      foreignKey: {
+        name: "user1",
+        allowNull: false
+      }
+    });
+    Match.belongsTo(models.User, {
+      as: "userInfo2",
+      foreignKey: {
+        name: "user2",
+        allowNull: false
+      }
+    });
   };
+
+  return Match;
+};
