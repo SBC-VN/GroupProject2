becc462221fed2aead47f012179d854f62e9c6c5
O <josephvesleywork@gmail.com>
2019-05-28 17:41:19 -0400

    scoring/matching/sorting

--- routes/match-api-routes.js
+++ routes/match-api-routes.js
@@ -1,30 +1,21 @@
 var db = require("../models");
 var mLogic=require("../routes/match-logic.js");
 var sLogic=require("../routes/score-logic.js");
-var users=[];
+var userAPI=require("../routes/user-api-routes.js");
 var path = require('path');
 
 
 module.exports = function(app) {
   app.get("/api/match", function(req, res) {
-    db.Users.findAll({}).then(function(dbMatches) {
+    db.User.findAll({}).then(function(dbMatches) {
 
-      users=dbUsers;
+      users=dbMatches;
       // console.log(dbUsers);
-      mLogic.populateScores(dbUsers);
+      mLogic.populateScores(dbMatches);
 
       for (var x=0;x<dbMatches.length;x++) {
         mLogic.matchUser(dbMatches[x]);
-        //   console.log(
-      //     "First Name: "+dbUsers[x].firstname +   " ".repeat(mLogic.colSpacer(dbUsers[x].firstname))  + " Score:" + dbUsers[x].sentimentScore
-      //   ); //+"\n")
-      //   var closeArr = mLogic.calculateMatches(dbUsers[x]);
-      //   var matches = mLogic.getMatches(closeArr);
-      //   // console.log(matches);
-      //   tempMatches = JSON.stringify(matches);
-      //   dbUsers[x].matches = JSON.parse(tempMatches);
-      //   // console.log(dbUsers[x].matches);
-      //   // dbUsers[x].matches=users[x].matches;
+
       }
 
       res.json(dbMatches);
@@ -48,3 +39,16 @@ module.exports = function(app) {
   });
 
 };
+
+
+//*************************************************************************************************************** */
+        //   console.log(
+      //     "First Name: "+dbUsers[x].firstname +   " ".repeat(mLogic.colSpacer(dbUsers[x].firstname))  + " Score:" + dbUsers[x].sentimentScore
+      //   ); //+"\n")
+      //   var closeArr = mLogic.calculateMatches(dbUsers[x]);
+      //   var matches = mLogic.getMatches(closeArr);
+      //   // console.log(matches);
+      //   tempMatches = JSON.stringify(matches);
+      //   dbUsers[x].matches = JSON.parse(tempMatches);
+      //   // console.log(dbUsers[x].matches);
+      //   // dbUsers[x].matches=users[x].matches;
