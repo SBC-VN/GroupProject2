a5eff9cd083aaddb90c4aa2cccde805f2e37f16d
SBC-VN <vnestohr@juno.com>
2019-05-30 20:26:32 -0400

    Modified database match read logic: 1. need to call the API with the user id of the user to get the matches for.  2.  Modified the model  3. Modified the sequelize call in the API to have a where and include.

--- routes/match-api-routes.js
+++ routes/match-api-routes.js
@@ -6,18 +6,26 @@ var path = require('path');
 
 
 module.exports = function(app) {
-  app.get("/api/matches", function(req, res) {
+  app.get("/api/matches/:id", function(req, res) {
+
+    // The 'where' clause limits the matches to the current user.  Without it ALL the matches
+    // for ALL the users will show up.   
+    // The 'include' clause pulls the matched user's info so that we can display it.
+    
+    // Joseph, do not delete or modify the where or include clauses.  Or the data model upon
+    //   which then depend.
+
+    // Joseph, do not delete or modify the where or include clauses.  Or the data model upon
+    //   which then depend.
     db.match.findAll({
-      
+      where: { user1: req.params.id },
+      include: [ {model: db.user, as: 'userInfo2'} ]      
     }).then(function(dbMatches) {
       res.json(dbMatches);
     });
+    // Joseph, do not delete or modify the where or include clauses.
   });
 
-
-
-  //    where: user1===req.body.user1
-   
   app.post("/api/match", function(req, res) {
     db.match.create(req.body).then(function(dbMatch) {
       res.json(dbMatch);
