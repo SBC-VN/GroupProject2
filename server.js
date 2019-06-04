581b558aa6551ffe67386d6eae82004f689902fe
HermesCat <jppettis@comcast.net>
2019-06-04 18:36:07 -0400

    fixed

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ require("./routes/user-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force = true }).then(function () {
+db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
     console.log("Match App listening on http://localhost:" + PORT);
   });
